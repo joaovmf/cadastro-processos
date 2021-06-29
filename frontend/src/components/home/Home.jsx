77cde91169c4b18b46672e9d908b1294a23eb9bb
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-06-29 10:57:22 -0300

    Criação projeto cadastro de processos.

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo Projeto do capítulo de React.">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sou advogado e atualmente estudo análise e desenvolvimento de sistemas, aprendendo a
+            desenvolver sistemas em diversas linguagens de programação. Criei essa ferramenta para me ajudar no 
+            cadastro dos meus processos e clientes. Sistema desenvolvido em react.</p>
+    </Main>
